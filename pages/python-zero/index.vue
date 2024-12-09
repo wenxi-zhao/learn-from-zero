f1931bc14549060765f00e0609abcf75cc9fe275
赵文希 <124047665+wenxi-zhao@users.noreply.github.com>
2024-12-09 20:35:59 +0800

    Delete pages/python-zero directory

--- pages/python-zero/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <ContentNavigation v-slot="{ navigation }" :query="queryContent('/python-zero/')">
-    <TutNav :nav="navigation[0].children" />
-  </ContentNavigation>
-</template>
--- pages/python-zero/test-page.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <CommentArea />
-</template>
